8615cc8fa726aa1c0a24a18e67b245c92d66bef5
mohamed bakayoko <mowamedbakus@gmail.com>
2019-05-25 22:56:32 +0000

    adding signup form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatInputModule],
+  exports: [MatInputModule]
 })
 export class MaterialModule { }
