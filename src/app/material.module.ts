a0d42781f2a8cbc5a0beb61ceb8c1863eb602c70
mohamed bakayoko <mowamedbakus@gmail.com>
2019-05-27 00:52:39 +0000

    adding date picker and vlidation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  imports: [MatInputModule, MatButtonModule],
-  exports: [MatInputModule, MatButtonModule]
+  imports: [
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ]
 })
 export class MaterialModule { }
